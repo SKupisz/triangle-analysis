f9c4846aee2ac6f0651ffda88e102e2ed42186c2
SKupisz <szym-ku@wp.pl>
2021-02-28 21:21:16 +0100

    Corrected about us section data

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ export default class About extends React.Component{
             <section className="about-section block-center">
                 <header className="about-header block-center">O programie</header>
                 <div className="purpose-desc block-center">Program wykonany na zaliczenie projektu końcoworocznego w XVIII liceum Ogólnokształcącym im. Jana Zamoyskiego w Warszawie.</div>
-                <div className="repo-adress">Github Repo: <a className = "github-link" href = "https://google.com">a</a></div>
+                <div className="repo-adress">Github Repo: <a className = "github-link" href = "https://github.com/SKupisz/triangle-analysis">triangle-analysis</a></div>
             </section>
         )
     }
